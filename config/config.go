4f4ba5afdae9c437a22e61bacb71c3fa535873b5
Ken Low <kenlow@fpgberhad.com>
2022-09-21 14:45:46 +0800

    Update env variable

--- config/config.go
+++ config/config.go
@@ -7,12 +7,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var DB_HOST string
-var DB_PORT string
-var DB_USER string
-var DB_PASSWORD string
-var DB_NAME string
 var SECRET string
+var DATABASE_URL string
 
 func LoadEnv() {
 	err := godotenv.Load()
@@ -20,11 +16,7 @@ func LoadEnv() {
 		log.Fatal("Error loading .env file")
 	}
 
-	DB_HOST = os.Getenv("DB_HOST")
-	DB_PORT = os.Getenv("DB_PORT")
-	DB_USER = os.Getenv("DB_USER")
-	DB_PASSWORD = os.Getenv("DB_PASSWORD")
-	DB_NAME = os.Getenv("DB_NAME")
+	DATABASE_URL = os.Getenv("DATABASE_URL")
 	SECRET = os.Getenv("SECRET")
 
 }
--- database/connect.go
+++ database/connect.go
@@ -4,7 +4,6 @@ import (
 	"crm_backend/config"
 	"crm_backend/model"
 	"fmt"
-	"strconv"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,12 +11,8 @@ import (
 
 func ConnectDB() {
 	var err error
-	p := config.DB_PORT
-	port, _ := strconv.ParseUint(p, 10, 32)
-	// dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", config.DB_HOST, port, config.DB_USER, config.DB_PASSWORD, config.DB_NAME)
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", config.DB_USER, config.DB_PASSWORD, config.DB_HOST, port, config.DB_NAME)
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(config.DATABASE_URL), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
